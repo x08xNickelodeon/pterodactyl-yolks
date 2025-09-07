da57238eb53b0653de2cc110f16e7b758fd3c54a
x08xNickelodeon <x08xNickelodeon@gmail.com>
2025-09-07 05:21:28 -0500

    Update entrypoint.sh

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -234,39 +234,38 @@ if [[ "$OVERRIDE_STARTUP" == "1" ]]; then
 	fi
 
 	if [[ "$ADDITIONAL_FLAGS" == "Aikar's Flags" ]]; then
-    FLAGS+=(
-        "-XX:+DisableExplicitGC"
-        "-XX:+ParallelRefProcEnabled"
-        "-XX:+PerfDisableSharedMem"
-        "-XX:+UnlockExperimentalVMOptions"
-        "-XX:+UseG1GC"
-        "-XX:G1HeapRegionSize=8M"
-        "-XX:G1HeapWastePercent=5"
-        "-XX:G1MaxNewSizePercent=40"
-        "-XX:G1MixedGCCountTarget=4"
-        "-XX:G1MixedGCLiveThresholdPercent=90"
-        "-XX:G1NewSizePercent=30"
-        "-XX:G1RSetUpdatingPauseTimePercent=5"
-        "-XX:G1ReservePercent=20"
-        "-XX:InitiatingHeapOccupancyPercent=15"
-        "-XX:MaxGCPauseMillis=200"
-        "-XX:SurvivorRatio=32"
-        "-Dusing.aikars.flags=https://mcflags.emc.gs"
-        "-Daikars.new.flags=true"
-    )
-	
+        FLAGS+=(
+            "-XX:+DisableExplicitGC"
+            "-XX:+ParallelRefProcEnabled"
+            "-XX:+PerfDisableSharedMem"
+            "-XX:+UnlockExperimentalVMOptions"
+            "-XX:+UseG1GC"
+            "-XX:G1HeapRegionSize=8M"
+            "-XX:G1HeapWastePercent=5"
+            "-XX:G1MaxNewSizePercent=40"
+            "-XX:G1MixedGCCountTarget=4"
+            "-XX:G1MixedGCLiveThresholdPercent=90"
+            "-XX:G1NewSizePercent=30"
+            "-XX:G1RSetUpdatingPauseTimePercent=5"
+            "-XX:G1ReservePercent=20"
+            "-XX:InitiatingHeapOccupancyPercent=15"
+            "-XX:MaxGCPauseMillis=200"
+            "-XX:SurvivorRatio=32"
+            "-Dusing.aikars.flags=https://mcflags.emc.gs"
+            "-Daikars.new.flags=true"
+        )
 
-    # Set MaxTenuringThreshold depending on Java version
-    if [[ "$JAVA_MAJOR_VERSION" -eq 8 ]]; then
-        FLAGS+=("-XX:MaxTenuringThreshold=8")
-    else
-        FLAGS+=("-XX:MaxTenuringThreshold=16")
-    fi
-	fi
+        # ✅ Only add MaxTenuringThreshold here
+        if [[ "$JAVA_MAJOR_VERSION" -eq 8 ]]; then
+            FLAGS+=("-XX:MaxTenuringThreshold=8")
+        else
+            FLAGS+=("-XX:MaxTenuringThreshold=16")
+        fi
 
-    # Only add AlwaysPreTouch if PRETOUCH=1
-    if [[ "$PRETOUCH" == "1" ]]; then
-        FLAGS+=("-XX:+AlwaysPreTouch")
+        # Only add AlwaysPreTouch if PRETOUCH=1
+        if [[ "$PRETOUCH" == "1" ]]; then
+            FLAGS+=("-XX:+AlwaysPreTouch")
+        fi
     fi
 
 
